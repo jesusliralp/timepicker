85048e369bd7955d73ee465beb55bd5d79db46c2
jfernando.lirap <jfernando.lirap@gmail.com>
2024-03-13 00:09:43 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'timepicker';
+export class AppComponent implements ControlValueAccessor {
+  @Input() militarTime: boolean = true;
+  @Input() step: number = 60;
+  @Input() value: number = 0;
+
+  private _onChange: any;
+  private _onTouched: any;
+
+  private _isDisabled: boolean = false;
+
+  writeValue(value: number): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this._isDisabled = isDisabled;
+  }
+
+  getSelectedTime(): string {
+    const seconds = this.getSeconds();
+    return `${this.getHours()}:${this.getMinutes()}${(!!seconds ? `:${seconds}` : '')}`
+  }
+
+  private getHours(): string {
+    return !!this.value ? Math.floor(this.value / 3600).toString().padStart(2, '-') : '--';
+  }
+
+  private getMinutes(): string {
+    const remaining = this.value % 3600;
+    return !!remaining ? Math.floor(remaining / 60).toString().padStart(2, '-') : '--';
+  }
+
+  private getSeconds(): string | null {
+    const remaining = this.value % 60;
+    return (this.step % 60 > 0) ? !!remaining ? remaining.toString().padStart(2, '-') : '--' : null;
+  }
 }
